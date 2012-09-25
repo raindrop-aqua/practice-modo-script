b9e5fb8671676fc6494d7f6dcfaca361b1997687
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-25 15:09:57 +0900

    Add placeholder samples

--- hello/hellomodo.py
+++ hello/hellomodo.py
@@ -1,7 +1,18 @@
 #python
 
+import urllib2
+
+# standard
 lx.out("Hello modo!")
+
+# concat
 lx.out("Python version:", sys.version)
+
+# dir
 lx.out("lx:", dir(lx))
 lx.out("sys:", dir(sys))
+lx.out("urllib2:", dir(urllib2))
 
+# placeholder
+lx.out("%s is strings" % "abcde")
+lx.out("%d %%" % 50)
