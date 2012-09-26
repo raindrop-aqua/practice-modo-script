75b1bfc925cc57128e91a1013e33caa2eaad1e67
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-26 23:54:49 +0900

    Add callback method

--- net/download.py
+++ net/download.py
@@ -2,6 +2,12 @@
 
 import urllib
 
-urllib.urlretrieve("http://download.thinkbroadband.com/10MB.zip", "e:/10MB.zip")
+url = "http://download.thinkbroadband.com/10MB.zip"
+file_name = "e:/10MB.zip"
+# call back
+def progress(block_count, block_size, total_size):
+    percentage = int(100.0 * block_count * block_size / total_size)
+    lx.out("%10d [%6.2f%%]" % (block_count * block_size / 1024, percentage))
 
+urllib.urlretrieve(url, file_name, progress)
 lx.out("download done.")
