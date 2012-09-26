1450d4f62c6e7825bd0c3befaf91af0ee4cdef59
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-26 23:55:32 +0900

    Change output is modo style

--- net/download2.py
+++ net/download2.py
@@ -1,19 +1,17 @@
 #python
 
 import urllib2
-import lx
 
 url_string = "http://download.thinkbroadband.com/10MB.zip"
 
-file_name = url_string.split('/')[-1]
+file_string = url_string.split('/')[-1]
 url = urllib2.urlopen(url_string)
-file = open("e:/dev/" + file_name, 'a+')
-meta = url.info()
-file_size = int(meta.getheaders("Content-Length")[0])
+file = open("e:/" + file_string, 'w+')
+file_size = int(url.info().getheaders("Content-Length")[0])
 
-lx.out("Downloading:%s / Bytes:%s" % (file_name, file_size))
+lx.out("Downloading:%s / Bytes:%s" % (file_string, file_size))
 
-file_size_downloaded = 0
+downloaded = 0
 block_size = 8192
 
 while True:
@@ -21,11 +19,10 @@ while True:
     if not buffer:
         break;
 
-    file_size_downloaded += len(buffer)
+    downloaded += len(buffer)
     file.write(buffer)
-    progress = file_size_downloaded * 100. / file_size
-    status = "%10d [%6.2f%%]" % (file_size_downloaded, progress)
-    status = status + "O" * (int(progress / 10))
+    progress = downloaded * 100.0 / file_size
+    status = "%10d [%6.2f%%]" % (downloaded, progress)
     lx.out(status)
 
 file.close()
